2dc0722c9678bb258cb74be4fdad714652105b67
Deepesh Patel <166607024+Deepeshpatel11@users.noreply.github.com>
2025-03-19 21:26:29 +0000

    Added template structure and basic views for all apps

--- labour_management/labour_management/settings.py
+++ labour_management/labour_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-w&%6i+8&53i14g@_)dvr*8wmcv24fx)1wswy77yxyr_b$kn5u%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
--- labour_management/labour_management/urls.py
+++ labour_management/labour_management/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("employees/", include("employees.urls")),
+    path("shifts/", include("shifts.urls")),
+    path("holidays/", include("holidays.urls")),
+    path("reports/", include("reports.urls")),
 ]
